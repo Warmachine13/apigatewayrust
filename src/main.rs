51e2f7ac9af32a64766eac004731e2cbb6c3b1c1
Lucas <lucas.souza@edulabzz.com.br>
2024-03-29 15:18:59 -0300

    fix: get configurtion file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder, Result};
+use actix_web::http::Uri;
+use actix_web::{get, post, web, App, HttpRequest, HttpResponse, HttpServer, Responder, Result};
 use hyper_util::rt::TokioIo;
 use path_tree::PathTree;
 use serde::{Deserialize, Serialize};
@@ -22,7 +23,7 @@ use std::path::Path;
 // use support::TokioIo;
 
 use http_body_util::{BodyExt, Empty};
-use hyper::{Request, Uri};
+use hyper::Request;
 // use hyper::{body::HttpBody as _, Client, Uri};
 use tokio::io::{self, AsyncWriteExt as _};
 use tokio::net::TcpStream;
@@ -32,12 +33,12 @@ use tokio::net::TcpStream;
 
 // type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Serialize, Debug)]
 struct User {
     routes: Vec<Route>,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Serialize, Debug)]
 struct Route {
     host: String,
 }
@@ -78,6 +79,8 @@ fn read_user_from_file<P: AsRef<Path>>(path: P) -> Result<User, Box<dyn Error>> 
 // async fn fetch_url(url: hyper::Uri) -> Result<()> {}
 
 async fn fetch_url(url: hyper::Uri) -> Result<()> {
+    println!("entrou fetch");
+    println!("{}", url);
     let host = url.host().expect("uri has no host");
     let port = url.port_u16().unwrap_or(80);
     let addr = format!("{}:{}", host, port);
@@ -128,39 +131,71 @@ async fn fetch_url(url: hyper::Uri) -> Result<()> {
     Ok(())
 }
 
-async fn index_handler(req: HttpRequest, routesnew: Arc<Vec<Route>>) -> impl Responder {
-    // let routes = data.get_ref();
-    let path = req.match_info().query("tail");
-    println!("{}", path);
-    println!("req {}", req.uri());
+// async fn index_handler(req: HttpRequest, routesnew: Arc<Vec<Route>>) -> impl Responder {
+//     // let routes = data.get_ref();
+//     let path = req.match_info().query("tail");
+//     println!("{}", path);
+//     println!("req {}", req.uri());
 
-    for route in routesnew.iter() {
-        // Clone the host for the closure
-        let route_host = route.host.clone();
-        // Create an Arc for the host to ensure it lives long enough
-        let route_host_arc = Arc::new(route_host);
-        // Create a clone for the async block
-        let uri = format!("http://{}", route_host_arc);
-        let uri_clone = uri.clone();
-        // Spawn async block with a new Arc reference
-        let handle = tokio::spawn(async move {
-            let uri = uri_clone.parse::<Uri>().expect("Failed to parse URI");
-            if let Err(err) = fetch_url(uri).await {
-                eprintln!("Failed to fetch URL {}: {}", route_host_arc, err);
-            }
-        });
-        // Ensure the handle doesn't drop until the end of the loop iteration
-        handle.await;
-    }
+//     for route in routesnew.iter() {
+//         // Clone the host for the closure
+//         let route_host = route.host.clone();
+//         // Create an Arc for the host to ensure it lives long enough
+//         let route_host_arc = Arc::new(route_host);
+//         // Create a clone for the async block
+//         let uri = format!("http://{}", route_host_arc);
+//         let uri_clone = uri.clone();
+//         // Spawn async block with a new Arc reference
+//         let handle = tokio::spawn(async move {
+//             let uri = uri_clone.parse::<Uri>().expect("Failed to parse URI");
+//             if let Err(err) = fetch_url(uri).await {
+//                 eprintln!("Failed to fetch URL {}: {}", route_host_arc, err);
+//             }
+//         });
+//         // Ensure the handle doesn't drop until the end of the loop iteration
+//         handle.await;
+//     }
 
+//     HttpResponse::Ok().body("Hey there!")
+// }
+
+#[get("/{tail:.*}")]
+async fn hello(req: HttpRequest) -> String {
+    let routes = read_user_from_file("routes.json").unwrap();
+    let mounted = serde_json::to_string(&routes).unwrap();
+    let parsed: serde_json::Value = serde_json::from_str(&mounted).unwrap();
+    // let path = req.match_info().query("tail");
+    // println!("{}", parsed["routes"][0]["host"]);
+    let mounted_url = parsed["routes"][0]["host"].to_string();
+    // .parse::<hyper::Uri>()
+    // .unwrap();
+    println!("req {}", mounted_url);
+    // fetch_url(mounted_url).await;
+    // HttpResponse::Ok().body(parsed["routes"][0]["host"])
+    parsed["routes"][0]["host"].to_string()
+}
+
+#[get("/configuration")]
+async fn configuration() -> String {
+    let routes = read_user_from_file("routes.json").unwrap();
+    let mounted = serde_json::to_string(&routes).unwrap();
+    mounted
+}
+
+#[post("/{tail:.*}")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let routes = read_user_from_file("routes.json").unwrap();
-    let routesnew = Arc::new(read_user_from_file("routes.json").unwrap().routes);
-    println!("{:#?}", routes);
+    // let routes = read_user_from_file("routes.json").unwrap();
+    // let routesnew = Arc::new(read_user_from_file("routes.json").unwrap().routes);
+    // println!("{:#?}", routesnew);
     // // Lê as rotas do arquivo JSON
     // let file = File::open("routes.json").expect("Failed to open routes.json");
     // let reader = BufReader::new(file);
@@ -179,43 +214,53 @@ async fn main() -> std::io::Result<()> {
     // }
     // let cloned_routes = routes.routes.clone();
     HttpServer::new(move || {
-        App::new().service(
-            web::resource("/{tail:.*}")
-                //.get(|| HttpResponse::Ok()),
-                .route(web::get().to(|req: HttpRequest| {
-                    let path = req.match_info().query("tail");
-                    println!("{}", path);
-                    println!("req {}", req.uri());
-                    // index_handler(req, routesnew.clone());
-                    // for route in routesnew.iter() {
-                    //     // Clone the host for the closure
-                    //     let route_host = route.host.clone();
-                    //     // Create an Arc for the host to ensure it lives long enough
-                    //     let route_host_arc = Arc::new(route_host);
-                    //     // Create a clone for the async block
-                    //     let uri = format!("http://{}", route_host_arc);
-                    //     let uri_clone = uri.clone();
-                    //     // Spawn async block with a new Arc reference
-                    //     let handle = tokio::spawn(async move {
-                    //         let uri = uri_clone.parse::<Uri>().expect("Failed to parse URI");
-                    //         if let Err(err) = fetch_url(uri).await {
-                    //             eprintln!("Failed to fetch URL {}: {}", route_host_arc, err);
-                    //         }
-                    //     });
-                    //     // Ensure the handle doesn't drop until the end of the loop iteration
-                    //     handle
-                    // }
-                    // for route in &routes.routes {
-                    //     fetch_url(route).await;
-                    // }
-
-                    HttpResponse::Ok()
-                    // match path_tree.find(&path) {
-                    //     Some(handler) => handler(req),
-                    //     None => HttpResponse::NotFound().body("Not Found"),
-                    // }
-                })),
-        )
+        App::new()
+            .service(echo)
+            .service(configuration)
+            .service(hello)
+            .route("/hey", web::get().to(manual_hello))
+            .service(
+                web::resource("/{tail:.*}")
+                    //.get(|| HttpResponse::Ok()),
+                    .route(web::get().to(|req: HttpRequest| {
+                        let path = req.match_info().query("tail");
+                        println!("{}", path);
+                        println!("req {}", req.uri());
+                        // println!("{:#?}", routesnew);
+                        // index_handler(req, routesnew.clone());
+                        let routesnew =
+                            Arc::new(read_user_from_file("routes.json").unwrap().routes);
+                        for route in routesnew.iter() {
+                            println!("{:#?}", route);
+                        }
+                        //     // Clone the host for the closure
+                        //     let route_host = route.host.clone();
+                        //     // Create an Arc for the host to ensure it lives long enough
+                        //     let route_host_arc = Arc::new(route_host);
+                        //     // Create a clone for the async block
+                        //     let uri = format!("http://{}", route_host_arc);
+                        //     let uri_clone = uri.clone();
+                        //     // Spawn async block with a new Arc reference
+                        //     let handle = tokio::spawn(async move {
+                        //         let uri = uri_clone.parse::<Uri>().expect("Failed to parse URI");
+                        //         if let Err(err) = fetch_url(uri).await {
+                        //             eprintln!("Failed to fetch URL {}: {}", route_host_arc, err);
+                        //         }
+                        //     });
+                        //     // Ensure the handle doesn't drop until the end of the loop iteration
+                        //     handle
+                        // }
+                        // for route in &routes.routes {
+                        //     fetch_url(route).await;
+                        // }
+                        // Ok(format!("Welcome {}!", info.username))
+                        HttpResponse::Ok()
+                        // match path_tree.find(&path) {
+                        //     Some(handler) => handler(req),
+                        //     None => HttpResponse::NotFound().body("Not Found"),
+                        // }
+                    })),
+            )
     })
     .bind("127.0.0.1:8080")?
     .run()
